359cc3f2d7da6d05e3fea260dd8861a7cb19fed8
dahekim <kimdre88@naver.com>
2022-08-03 17:03:45 +0900

    알고리즘: 문자열 내 p와 y의 갯수

---
+++ algorithm/04-p-y-strCounts.js
@@ -0,0 +1,38 @@
+// 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+// 예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+
+// 제한사항
+// 문자열 s의 길이 : 50 이하의 자연수
+// 문자열 s는 알파벳으로만 이루어져 있습니다.
+
+function solution(s){
+    let pNums = 0;
+    let yNums = 0;
+    
+    const arr = s.toLowerCase().split("");
+    
+    // p와 y가 하나도 없는 경우
+    if(arr.includes('p') === false
+       && arr.includes('y') === false) {
+        return true;
+    }
+    
+    // p나 y가 포함된 경우
+    if(arr.includes('p') || arr.includes('y')){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] === 'p'){
+                pNums += 1;
+            }
+            
+            if(arr[i] === 'y'){
+                yNums += 1;
+            }
+        }
+        console.log(pNums, yNums)
+        if(pNums === yNums){
+            return true;
+        }
+    }
+    return false;
+}
