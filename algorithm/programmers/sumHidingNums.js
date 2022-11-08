1c99ed5b561a71139fc11675f167a73ebedba214
dahekim <kimdre88@naver.com>
2022-11-08 17:15:03 +0900

    프로그래머스 코딩테스트: 숨어있는 숫자의 덧셈(2)

--- algorithm/programmers/sumHidingNums.js
+++ algorithm/programmers/sumHidingNums.js
@@ -14,11 +14,10 @@ function solution(my_string) {
     // my_string이 "aAb1B2cC34oOp" 이라면 nums = [ '1', '2', '34' ]
     const nums = my_string.match(/[0-9]+/g);
 
-
     // 2. map()을 이용해서 nums 배열의 요소를 전부 string형태에서 number 형태로 바꾸고
     // 3. reduce()를 이용하여 배열의 요소를 더하여 반환한다.
     return nums ? nums
-                        .map(num => Number(num))
+                      .map(num => Number(num))
                       .reduce( (a, c) => a + c, 0) 
                 : 0;
 }
